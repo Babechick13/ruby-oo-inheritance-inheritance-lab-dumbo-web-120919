93d2b48169cae8133e54f11af839132476bd9e88
Jessica Natovich <jessicanatovich@yahoo.com>
2019-12-17 22:25:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,7 @@
-class Student 
-  attr_reader :user
-  def initialize (user)
-  @user=user
-end
-end
+avi = Teacher.new
+avi.first_name = "Avi"
+avi.last_name = "Flombom"
+avi.last_name = "Flombaum"
+
+
+some_knowledge = avi.teach 
